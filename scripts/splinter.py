393aef02725671ccf5f1fc5112f1b16624cdc99b
zxcfer <zxcfer@proton.me>
2024-06-21 19:56:10 -0500

    pdf splitter finished

--- scripts/splinter.py
+++ scripts/splinter.py
@@ -1,15 +1,35 @@
+import os
 from PyPDF2 import PdfReader, PdfWriter
 
-file_name = r'c:\temp\junk.pdf'
-pages = (121, 130)
+# dir_path = 'D:/tmp/pdfs/'
+# extension = '.pdf'
 
-reader = PdfReader(file_name)
-writer = PdfWriter()
-page_range = range(pages[0], pages[1] + 1)
+# files = os.listdir(dir_path)
+# pdfs = [file for file in files if os.path.splitext(file)[1] == extension]
 
-for page_num, page in enumerate(reader.pages, 1):
-    if page_num in page_range:
-        writer.add_page(page)
+# # Print the list of matching files
+# print(pdfs)
 
-with open(f'{file_name}_page_{pages[0]}-{pages[1]}.pdf', 'wb') as out:
-    writer.write(out)
+# file_name = r'D:\*'
+# pages = (121, 130)
+
+import os
+from PyPDF2 import PdfReader, PdfWriter
+
+dir_path = 'D:/tmp/pdfs/'  # Directory containing PDFs
+extension = '.pdf'
+
+files = os.listdir(dir_path)
+pdfs = [file for file in files if os.path.splitext(file)[1] == extension]
+
+for pdf in pdfs:
+    reader = PdfReader(os.path.join(dir_path, pdf))
+    for i in range(len(reader.pages)):
+        writer = PdfWriter()
+        writer.add_page(reader.pages[i])
+        output_filename = f"{os.path.splitext(pdf)[0]}_page_{i+1}.pdf"
+        with open(os.path.join(dir_path, output_filename), 'wb') as output_pdf:
+            writer.write(output_pdf)
+
+# Print the list of matching files
+print(pdfs)
